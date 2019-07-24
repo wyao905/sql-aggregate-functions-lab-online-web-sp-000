93c66f6b0112d55cb1d54bf7bb0480f1cd2e3d9e
Warren Yao <warrenyca905@gmail.com>
2019-07-24 00:26:05 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,7 +1,7 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
 def highest_student_gpa
-  "SELECT MAX(gpa) FROM cats;"
+  "sqlite3 SELECT MAX(gpa) FROM cats;"
 end
 
 def lowest_student_gpa
